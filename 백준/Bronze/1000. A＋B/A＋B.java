2b99b4e64a9dc7de927dd86e9ee46d3e4de7dbaa
Hyojin Bae <110245772+Land1a@users.noreply.github.com>
2024-01-26 17:36:55 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = Integer.parseInt(scanner.next());
+        b = Integer.parseInt(scanner.next());
+        System.out.println(a+b);
+    }
+}
